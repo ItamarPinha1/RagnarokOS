3a6b21b11085fdda7ee06f5f17ccff22f9720ea9
ItamarPinha1 <itamarpinhas5@gmail.com>
2024-09-03 03:16:58 +0300

    save

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,7 +4,5 @@ void main()
 {
     ClearScreen();
 
-    char *name = "Itamar";
-
-    printf("My name %%f is %c and %c and for last im %s abc", 'a', 'b', name);
+    printf("Hello World! %s", "Taub");
 }
